b79311d02787c0787dbd0b84e2f3eb7ade0e21b0
dineshrao <dinesh.rao@civilmachines.com>
2018-07-14 13:06:36 +0530

    solve update profile issue,

--- src/app/component/profile/profile.component.ts
+++ src/app/component/profile/profile.component.ts
@@ -14,6 +14,9 @@ import {APICallService} from '../../service/api-service/apicall.service';
 })
 export class ProfileComponent extends SharedClass implements OnInit {
   title = 'My Profile';
+  name: string;
+  mobile: string;
+  email: string;
   constructor(private rtr: Router, private data: DataService, private navbar: NavbarService, private toast: ToastrService,
               public location: Location, private apiObject: APICallService) {
     super(rtr);
@@ -23,11 +26,15 @@ export class ProfileComponent extends SharedClass implements OnInit {
 
   ngOnInit() {
     super.ngOnInit();
+    this.name = this.s_name;
+    this.mobile = this.s_mobile;
+    this.email = this.s_email;
+
     /* To check if user is logged in or logged out, if logout then it will redirect user to login page */
    // if (!this.KEY_TOKEN) { this.rtr.navigate(['/', 'login']); }
   }
   onUpdate() {
-    this.apiObject.updateProfile(this.s_name, this.s_mobile, this.s_email).subscribe(
+    this.apiObject.updateProfile(this.name, this.mobile, this.email).subscribe(
       data => {
         this.toast.success('Your profile updated successfully', 'Profile');
       }, error => {
